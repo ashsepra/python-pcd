f2b60f5fe57efdaf06857dc3447441a00ea80d72
I Putu Ashadi Sedana Pratama <wb-utushadie283506@antfin.com>
2020-12-02 20:36:26 +0800

    add hvs

--- histogram/1929101037.py
+++ histogram/1929101037.py
@@ -9,10 +9,23 @@ from matplotlib import pyplot as plt
 
 # Get image from "images" directory
 # I'm using Lenna file for example
-file_img = './images/LC.jpeg'
-img_src = cv2.imread(file_img)
-plt.hist(img_src.ravel(),256,[0,256])
-plt.show()
+file_imgs = ['GS.png', 'GS-Bright.jpeg', 'GS-Dark.jpeg', 'Lenna.png', 'Lenna-Bright.jpeg', 'Lenna-Dark.jpeg', 'HC.jpeg', 'LC.jpeg']
+for file_img in file_imgs:
+  img_src = cv2.imread('./images/' + file_img)
+
+  plt.title(file_img)
+  plt.hist(img_src.ravel(),256,[0,256])
+  plt.show()
+
+file_imgs = ['Lenna.png', 'Lenna-Bright.jpeg', 'Lenna-Dark.jpeg', 'HC.jpeg', 'LC.jpeg']
+for file_img in file_imgs:
+  img_src = cv2.imread('./images/' + file_img)
+  img_convert = cv2.cvtColor(img_src, cv2.COLOR_BGR2HSV)
+  cv2.imwrite('./images/hsv/' + file_img, img_convert)
+
+  plt.title(file_img)
+  plt.hist(img_convert.ravel(),256,[0,256])
+  plt.show()
 
 cv2.waitKey(0)
 cv2.destroyAllWindows()
